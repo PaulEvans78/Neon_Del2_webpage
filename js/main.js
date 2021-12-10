8a124dfef9fa3308a64cd10844ff7f41ddb08070
Paul Evans <paulevansdop@gmail.com>
2021-12-10 14:56:44 +0100

    Hire_inital_layout_done+ remerge_folders

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 var closebtns = document.getElementsByClassName("close");
 var i;
 
 /* Loop through the elements, and hide the parent, when clicked on */
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
-    this.parentElement.style.display = 'none';
-  });
-}
+// for (i = 0; i < closebtns.length; i++) {
+//   closebtns[i].addEventListener("click", function() {
+//     this.parentElement.style.display = 'none';
+//   });
+// }
